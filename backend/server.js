1156b395967e34cae16ae94aa4d86bf56637bdc2
anisha-singhal <anishasinghal15@gmail.com>
2025-09-11 23:36:11 +0530

    feat(backend): Add /api/triage endpoint stub

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const PORT = 3001
 
-app.get('/api/test', (req, res) => {
-    res.json({ message: 'Backend is running!'});
+app.use(cors())
+app.use(express.json()) // Allow backend to accept JSON from the frontend
+
+app.post('/api/triage', (req, res) => {
+  const { emailText } = req.body;
+  console.log('Received email text:', emailText);
+
+    const fakeTriageReport = {
+    classification: 'Billing Issue',
+    priority: 'High',
+    suggested_reply: 'This is a test reply from the backend.'
+  };
+
+  res.json(fakeTriageReport);
 })
 
 app.listen(PORT, () => {
